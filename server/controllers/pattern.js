6121a128d22c31bb3e35f6a902d39f7081a0d381
Sam VanTassel <vantassel.sam@gmail.com>
2021-12-08 11:12:56 -0800

    \!refactor: converted saving new pattern to middleware format, reused loadPattern middleware. Update patern not implemented, program will not work right now. Need to finish converting updatePattern to middleware.

--- server/controllers/pattern.js
+++ server/controllers/pattern.js
@@ -3,7 +3,32 @@ import wordList from '../static/words.js';
 
 const usedWords = [];
 
+const generateRandomWord = (req, res, next) => {
+  const randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+  while (usedWords.includes(randomWord)) { randomWord = wordList[Math.floor(Math.random() * wordList.length)];}
+  usedWords.push(randomWord);
+  res.locals.randomWord = randomWord;
+  return next();
+}
+
+const loadPattern = (req, res, next) => {
+  const loadSlot = { slot: req.params.slot } 
+  Pattern.findOne(loadSlot)
+    .then(found => {
+      res.locals = { 
+        pattern: found ? found.pattern : new String('.').repeat(16),
+        name: found ? found.name : 'xxxx',
+      };
+      return next();
+    })
+    .catch(err => {
+      console.log(err)
+      return next(err);
+    })   
+};
+
 const savePattern = (req, res, next) => {
+  // DEPRECATED !!!!!!!!!!!!!!!
   /*
     UPDATE PATTERN LOGIC:
     0) [X] If there are no documents with the sent slot, create a new one with slot, pattern and name
@@ -11,7 +36,7 @@ const savePattern = (req, res, next) => {
     2) [X] If slot exists and sent pattern is same as pattern found at slot, don't update
     3) [X] If slot exists and sent pattern is different than pattern found at slot, update pattern and name
   */
- 
+
   // FIND DOCUMENT MATCHING SLOT
   Pattern.findOne({ slot: req.params.slot })
     .then(stored => {
@@ -31,30 +56,33 @@ const savePattern = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-const generateRandomWord = (req, res, next) => {
-  const randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-  while (usedWords.includes(randomWord)) { randomWord = wordList[Math.floor(Math.random() * wordList.length)];}
-  usedWords.push(randomWord);
-  res.locals.randomWord = randomWord;
-  return next();
+const saveNewPattern = (req, res, next) => {
+  if (res.locals.name === 'xxxx') { // IF NAME IS 'xxxx', CREATE NEW PATTERN W/ PASSED IN PATTERN AND RANDOM NAME
+    console.log('created new pattern!');
+    Pattern.create({ slot: req.params.slot, pattern: req.body.pattern, name: res.locals.randomWord })
+      .then((data) => {
+        res.locals.pattern = data.pattern;
+        res.locals.name = data.name;
+        res.locals.alreadySavedNew = true;
+        return next();
+      })
+      .catch(err => {
+        console.log(err)
+        return next(err);
+      })
+  } else return next();
 }
 
-const loadSlot = (req, res, next) => {
-  Pattern.findOne({ slot: req.params.slot })
-    .then(maybeFound => {
-      res.locals.found = maybeFound;
-      return next();
-    })
-    .catch(err => {
-      console.log(err)
-      return next(err);
-    })
-}
-const saveNewPattern = (req, res, next) => {
-  console.log('in saveNewPattern middleware. res.locals.maybeFound should be empty object string or null or something: ' + res.locals.maybeFound)
-  if (!res.locals.maybeFound) { // IF ONE DOESN'T EXIST, CREATE ONE W/ PASSED IN PATTERN AND RANDOM NAME
-    console.log('created new pattern!');
-    return Pattern.create({ slot: req.params.slot, pattern: req.body.pattern, name: randomWord })
+const updatePattern = (req, res, next) => {
+  if (res.locals.alreadySavedNew) return next();
+  else {
+    // FIX THIS SECTION!!!!!!! NOT GOING TO WORK!!!!!
+    else if (stored.pattern === req.body.pattern) { // IF DOC EXISTS BUT PATTERN IS SAME, DON'T UPDATE
+      console.log('Same pattern, passed on request body!')
+      return Promise.resolve(req.body);
+    } else {                               // IF DOC EXISTS AND PATTERN IS DIFFERENT, UPDATE PATTERN AND NAME
+      console.log('Updated doc with new pattern and name!')
+      return Pattern.findOneAndUpdate({ slot: req.params.slot }, { pattern: req.body.pattern, name: randomWord }, { new: true })
   }
 }
 
@@ -75,20 +103,4 @@ const deletePatternIfEmpty = (req, res, next) => {
   }
 }
 
-const loadPattern = (req, res, next) => {
-  const loadSlot = { slot: req.params.slot } 
-  Pattern.findOne(loadSlot)
-    .then(found => {
-      res.locals = { 
-        pattern: found ? found.pattern : new String('.').repeat(16),
-        name: found ? found.name : 'xxxx',
-      };
-      return next();
-    })
-    .catch(err => {
-      console.log(err)
-      return next(err);
-    })   
-};
-
-export { savePattern, deletePatternIfEmpty, loadPattern, generateRandomWord }
+export { deletePatternIfEmpty, loadPattern, generateRandomWord, saveNewPattern, updatePattern }
--- server/routes/memory.js
+++ server/routes/memory.js
@@ -1,19 +1,27 @@
 import express from 'express';
 const router = express.Router();
-import { loadPattern, savePattern, deletePatternIfEmpty, generateRandomWord } from '../controllers/pattern.js';
+import { loadPattern, deletePatternIfEmpty, generateRandomWord, saveNewPattern, updatePattern } from '../controllers/pattern.js';
 
 // ON GET REQUEST, LOAD PATTERN
 router.get('/:bank/:slot', 
   loadPattern,
-  (req, res) => res.json(res.locals)
+  (req, res) => res.json({
+    name: res.locals.name,
+    pattern: res.locals.pattern
+  })
 );
 
 // ON POST REQUEST, SAVE PATTERN
 router.post('/:bank/:slot', 
   deletePatternIfEmpty,
   generateRandomWord,
-  savePattern,
-  (req, res) => res.json(res.locals)
+  loadPattern,
+  saveNewPattern,
+  updatePattern,
+  (req, res) => res.json({
+    name: res.locals.name,
+    pattern: res.locals.pattern
+  })
 );
 
 export default router;
