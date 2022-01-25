ff7d16d5e725676adac0933758a5d06ad94176f9
Sam VanTassel <vantassel.sam@gmail.com>
2022-01-24 19:03:09 -0800

    build: change webpack output dir

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -47,7 +47,7 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, 'dist/'),
+    path: path.resolve(__dirname, 'public/'),
     publicPath: '/',
     filename: 'bundle.js',
   },
