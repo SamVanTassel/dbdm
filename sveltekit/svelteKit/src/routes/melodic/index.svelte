a8492aa5d3b763da19e38a5521d135c404ad057b
Sam VanTassel <vantassel.sam@gmail.com>
2022-03-10 14:22:58 -0800

    more sequencing patterns

--- sveltekit/svelteKit/src/routes/melodic/index.svelte
+++ sveltekit/svelteKit/src/routes/melodic/index.svelte
@@ -6,7 +6,7 @@
   let step: number = -1;
   let loaded: boolean = false;
 
-  const playbackStyles: string[] = ['standard', 'reverse']
+  const playbackStyles: string[] = ['standard', 'reverse', 'top-to-bottom', 'bottom-to-top']
   let playbackStyleIndex = 0;
   
   toneData.subscribe(data => {
@@ -18,6 +18,12 @@
   $: switch (playbackStyles[playbackStyleIndex]) {
     case 'reverse':
       funkyStep = 15 - step;
+      break;
+    case 'top-to-bottom':
+      funkyStep = (step * 4) % 16 + Math.floor(step / 4);
+      break;
+    case 'bottom-to-top':
+      funkyStep = 15 - ((step * 4) % 16 + Math.floor(step / 4));
       break;
     case 'standard':
       funkyStep = step;
@@ -38,7 +44,7 @@
 </script>
 
 <h2>temp</h2>
-<button on:click={switchPlayback}>Reverse</button>
+<button class="playbackSwap" on:click={switchPlayback}>Funk it up</button>
 <main class="grid">
   {#each tonalSteps as tonalStep, i}
     <button class={`bigButton ${i === funkyStep ? 'on' : ''}`}>{tonalStep.note}</button>
@@ -70,4 +76,8 @@
       background-color: rgb(240, 179, 99);
       color:rgb(201, 110, 77)
     }
+  .playbackSwap {
+    width: 100px;
+    height: 25px;
+  }
 </style>
